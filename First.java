5d5c4d2d7605c48dbf9b9a588347ee16405cf7a7
Sererim <danielleymanw@gmail.com>
2023-02-09 21:25:09 +0300

    First exercies is done.

--- First.java
+++ First.java
@@ -1,3 +1,37 @@
+import java.util.Scanner;
+
 public class First {
+
+
+    public static boolean ispalindrom(String word) {
+        String foo = new StringBuilder(word).reverse().toString();
+        if(word.equals(foo)) return true;
+        else return false;
+    }
+
+    public static void main(String[] args) {
+        String word = "";
+        Scanner scan = new Scanner(System.in);
+
+        while(true)
+        {
+            System.out.println("Enter a string.");
+            word = scan.nextLine();
     
+            if(First.ispalindrom(word))
+            {
+                System.out.printf("Entered string %s is palindrome.\n", word);
+            }
+            else
+            {
+                System.out.printf("Entered string %s isn't palindrome.\n", word);
+            }
+            
+            System.out.println("Do you want to terminate the program Y/y ?\n");
+            word = scan.nextLine();
+            if(word.equals("Y") || word.equals("y")) break;
+        }
+        scan.close();
+    }
 }
+ 
